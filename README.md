7c6e6706ce0ea7587c3411c535ee0b0f54aa2f37
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-02-04 19:35:32 +0200

    Add more parsing to enable SQL exporting

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # bible-parser2000
-A tool for removing titles and empty lines from a .txt bible.
+A tool for editing a particular .txt file so that its contents can be easily exported into an SQL database.
+The file "finnish_bible.txt" that this tool is made for is protected by copyright and is not included in the repository.
 
-To test this tool, run it with testbible.txt and see which lines stay and which don't.
+# How to use
+`python3 bparser.py`
 
-The "full_finnish_bible.txt" this tool is made for is in a funet FTP sever, use a search engine to find it.
+# Testing
+To test this script, run it with `testbible.txt` and compare the output `parsedbible.txt` with the original `testbible.txt`.
--- bparser.py
+++ bparser.py
@@ -1,7 +1,13 @@
 # 
-#              bible-parser2000
-# removes boring lines and titles from a bible
+#                  bible-parser2000
+#  * Removes empty lines and titles from a bible.
+#  * Arranges the remaining lines so that each line
+#       starts with a chapter and a verse.
+#  * Adds a delim after the first two words.
 # 
+#  Please use python3 instead of python2.7.
+#
+#
 # Empty lines are boring
 # -> no empty lines
 # Old testament titles are fully capitalized
@@ -11,11 +17,49 @@
 #
 
 filename = input("Enter your bible filename: ")
+delim =  input("Enter the delim for SQL exporting, space for no delim: ")
+
 with open(filename, "r") as bible, open("parsedbible.txt","w") as parsedbible:
 
+    previousline = ""
+
     for line in bible:
-    
+        
+        # ignore empty, tabulated and fully capitalized lines
         if line == "\n" or line.startswith("\t") or line.isupper():
             continue
-        parsedbible.write(line)
+        
+        line = line.lstrip() # remove white space from right side
+        line = line.replace("\n", "") # remove all endlines
+        
+        words = line.split()
+        
+        # If the current line is too short or
+        # the 2nd word is nort a verse (e.g. 12:34),
+        # append the current line to the previous line.
+        if len(words) < 3:
+            previousline = previousline + " " + line
+        elif ":" not in words[1] or words[1].replace(":", "").isalpha():
+            previousline = previousline + " " + line
+        
+        else:
+            
+            # make chapter name lowercase, e.g. 1Moos. -> 1moos.
+            line = line.replace(words[0], words[0].lower(), 1)
+            
+            if "." in words[0]:
+                # remove a period from chapter, e.g. 1moos. -> 1moos
+                line = line.replace(".", "", 1)
+            if "." in words[1]:
+                # remove a period from verse, e.g. 12:34. -> 12:34
+                line = line.replace(".", "", 1)
+            
+            line = line.replace(" ", delim, 2) # change two first spaces into %
+            if previousline != "":
+                parsedbible.write(previousline + "\n")
+            previousline = line
+    
+    # collect the last remaining line
+    parsedbible.write(previousline)
+            
         
